c502a9df70b1c83e1aa6f550b96e871607d3b77d
juanda <juandaob21@gmail.com>
2023-09-24 10:37:24 -0500

    first commit

---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Articulo;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $articulos = Articulo::all();
+        return $articulos;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $Articulo = new Articulo();
+        $Articulo->descripcion = $request->descripcion;
+        $Articulo->precio = $request->precio;
+        $Articulo->stock = $request->stock;
+
+        $Articulo->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $Articulo = Articulo::findOrFail($request->id);
+
+        $Articulo->descripcion = $request->description;
+        $Articulo->precio = $request->precio;
+        $Articulo->stock = $request->stock;
+
+        return $Articulo->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        $articulo = Articulo::destroy($request->id);
+        return $articulo;
+    }
+}
